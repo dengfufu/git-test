63ffc510d14330fada75a28752b63da424d2aade
fudeng <fudeng@zjft.com>
2020-08-11 14:59:52 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/dengfufu/FirstClass.java
@@ -0,0 +1,10 @@
+package com.dengfufu;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello DengFu!");
+        System.out.println("Hello DengFu!");
+        System.out.println("Hello DengFu!");
+        System.out.println("Hello DengFu!");
+    }
+}
